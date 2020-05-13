95a7ad4cf84aa117e3daf8bd08c96b185f908246
Antoine Augusti <antoine.augusti@data.gouv.fr>
2020-05-13 10:40:27 -0400

    Comment quality report for new files (#5)
    * Comment quality report for new files
    
    * Env variable for API domain
    
    * Use env variables in CI
    
    * Improve wording
    
    * Revert "Env variable for API domain"
    
    This reverts commit 51df7bee28e9332a2ea8046e1c0baebc7350d5b2.
    
    * Remove env from GH Action

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ import csv
 import datetime
 import sys
 import json
+import os
+import textwrap
+
+CSV_PATH = "data/data.csv"
 
 
 @functools.lru_cache()
@@ -18,6 +22,17 @@ def schemas_details():
     with requests.get("https://schema.data.gouv.fr/schemas/schemas.yml") as response:
         response.raise_for_status()
         return yaml.safe_load(response.content)
+
+
+@functools.lru_cache()
+def existing_data():
+    with open(CSV_PATH, "r") as f:
+        return [d for d in csv.DictReader(f)]
+
+
+def file_is_new(file_url):
+    same_line = [row for row in existing_data() if file_url == row["file_url"]]
+    return len(same_line) == 0
 
 
 def get_schema_url(slug):
@@ -121,6 +136,42 @@ def build_details(details, report):
     }
 
 
+def post_comment(details):
+    def plural(count, word):
+        if count != 1:
+            return f"{count} {word}s"
+        return f"{count} {word}"
+
+    schema_doc_url = f"https://schema.data.gouv.fr/{details['schema_slug']}/latest.html"
+
+    comment = f"""\
+    Bonjour,
+
+    Vous recevez ce message car ce jeu de données est une consolidation qui se veut conforme au schéma [{details['schema_slug']}]({schema_doc_url}), ce qui a déclenché un contrôle automatique de vos données par notre robot de validation.
+
+    [Le fichier]({details["file_url"]}) que vous venez de publier ou mettre à jour comporte {plural(details["nb_errors"], "erreur")} sur un total de {plural(details["nb_rows"], "ligne")} par rapport au [schéma de référence]({schema_doc_url}).
+
+    Vous pouvez consulter le [dernier rapport de validation]({details["report_url"]}) pour vous aider à corriger les erreurs.
+
+    Une fois un fichier valide publié, vous pouvez clore cette discussion.
+
+    Une question ? Écrivez à validation@data.gouv.fr en incluant l'URL du jeu de données concerné.
+    """
+
+    requests.post(
+        "https://www.data.gouv.fr/api/1/discussions/",
+        headers={
+            "X-API-KEY": os.environ["DATAGOUV_API_KEY"],
+            "User-Agent": "https://github.com/etalab/monitor-consolidation",
+        },
+        json={
+            "title": "Conformité au schéma",
+            "comment": textwrap.dedent(comment),
+            "subject": {"id": details["dataset_id"], "class": "Dataset"},
+        },
+    ).raise_for_status()
+
+
 daily_data = []
 json_report = {}
 for slug, data in schemas_details().items():
@@ -134,12 +185,17 @@ for slug, data in schemas_details().items():
 
         json_report[dataset_id] = build_report(details)
 
+        # If the file is new, post a comment on the dataset
+        # to report the validation's result
+        if file_is_new(details["file_url"]):
+            post_comment(details)
+
 # Write today's data to a JSON file
 with open("data/report.json", "w") as f:
     json.dump(json_report, f, indent=2, ensure_ascii=False)
 
 # Append daily data to a CSV file
-with open("data/data.csv", "a") as f:
+with open(CSV_PATH, "a") as f:
     writer = csv.DictWriter(f, daily_data[0].keys(), lineterminator="\n")
     if f.tell() == 0:
         writer.writeheader()
